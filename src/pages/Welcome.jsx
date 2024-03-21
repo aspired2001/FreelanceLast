eb122880da49256299e4668bd2fc79cb0f08df05
aspired2001 <100749624+aspired2001@users.noreply.github.com>
2024-03-21 14:47:44 +0530

    Carreers

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -12,21 +12,26 @@ const Welcome = ({animate}) => {
         <div className="container mx-auto px-16 mt-[70px]">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                     <div className="flex flex-col justify-center px-2 sm:px-2 md:px-2 lg:px-8">
-                        <h1 className="text-4xl md:text-3xl font-bold mb-4 text-blue-700 font-serif text-center pb-8">
+                        <h1 className="text-4xl md:text-3xl font-bold mb-4 text-blue-700 font-serif pb-4 sm:pb-6 lg:pb-8 text-center lg:-translate-x-12">
                             Welcome to Balaji Agro
                         </h1>
-                        <p className=" text-[18px] md:text-lg leading-relaxed text-justify font-serif w-full sm:w-auto">
-                            Our Company was incorporated as Balaji Agro Tech as a public limited company, under
-                            the Companies Act, 1956 vide Certificate of Incorporation dated
-                            July 31, 2008. Our Company is in the business of processing of
-                            non-basmati and basmati rice in India.
-                        </p>
-                        <p className="mt-4 text-[18px] md:text-lg leading-relaxed text-justify font-serif w-full sm:w-auto">
-                            We are one of the largest processors of non-basmati rice in the
-                            State of Jharkhand with a milling capacity of 1500 Metric tons per
-                            day, and we market our products across Eastern Indian States, namely
-                            Jharkhand, Bihar, Odisha, West Bengal, and Assam.
-                        </p>
+
+                        <div className="w-full sm:w-[80%]"> {/* Adjust width percentage as needed */}
+                            <p className="text-[18px] md:text-lg leading-relaxed text-justify font-serif">
+                                Our Company was incorporated as Balaji Agro Tech as a public limited company, under
+                                the Companies Act, 1956 vide Certificate of Incorporation dated
+                                July 31, 2008. Our Company is in the business of processing of
+                                non-basmati and basmati rice in India.
+                            </p>
+                        </div>
+                        <div className="mt-4 w-full sm:w-[80%]"> {/* Adjust width percentage as needed */}
+                            <p className="text-[18px] md:text-lg leading-relaxed text-justify font-serif">
+                                We are one of the largest processors of non-basmati rice in the
+                                State of Jharkhand with a milling capacity of 1500 Metric tons per
+                                day, and we market our products across Eastern Indian States, namely
+                                Jharkhand, Bihar, Odisha, West Bengal, and Assam.
+                            </p>
+                        </div>
                     </div>
                     <div className="relative mx-auto overflow-hidden w-full md:max-w-md lg:max-w-lg xl:max-w-xl">
                         <div className="relative z-10 overflow-hidden rounded-[18px] p-[2px] mt-[8px] w-full">
