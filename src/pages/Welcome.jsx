3c0e1b6af23154cbb8d038b9a60e91a4fb2bdc6c
aspired2001 <100749624+aspired2001@users.noreply.github.com>
2024-03-21 12:31:26 +0530

    Welcomes

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -7,36 +7,36 @@ const Welcome = ({animate}) => {
 
     return (
       
-        <div className={`transition-all duration-1000 pt-8 ${animate ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-10'}`}>
+        <div className={`transition-all duration-1000 pt-4 ${animate ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-10'}`}>
 
         <div className="container mx-auto px-16 mt-[70px]">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <div className="flex flex-col justify-center">
-                    <h1 className="text-5xl md:text-4xl font-bold mb-4 text-blue-700 font-serif text-center pb-8 ">
-                        Welcome to the Balaji Agro
-                    </h1>
-                    <p className="text-base md:text-lg leading-relaxed text-justify font-serif ">
-                        Our Company was incorporated as Balaji Agro Tech as a public limited company, under
-                        the Companies Act, 1956 vide Certificate of Incorporation dated
-                        July 31, 2008. Our Company is in the business of processing of
-                        non-basmati and basmati rice in India.
-                    </p>
-                    <p className="mt-4 md:text-lg leading-relaxed text-justify font-serif">
-                        We are one of the largest processors of non-basmati rice in the
-                        State of Jharkhand with a milling capacity of 1500 Metric tons per
-                        day, and we market our products across Eastern Indian States, namely
-                        Jharkhand, Bihar, Odisha, West Bengal, and Assam.
-                    </p>
-                </div>
-                <div className="md:block relative mx-auto overflow-hidden rounded-[10px] max-w-[450px]">
-                    <div className="relative z-10 overflow-hidden rounded-[18px] p-[2px] mt-[8px] w-full md:min-w-[350px] md:mr-14 md:-translate-x-8">
-                        <img
-                            src="https://images.pexels.com/photos/3345727/pexels-photo-3345727.jpeg?auto=compress&cs=tinysrgb&w=600"
-                            alt="Placeholder"
-                            className="w-[300px] h-[300px] md:h-[300px] object-cover object-center transform scale-100 transition-transform duration-300 hover:scale-105"
-                        />
+                    <div className="flex flex-col justify-center px-2 sm:px-2 md:px-2 lg:px-8">
+                        <h1 className="text-4xl md:text-3xl font-bold mb-4 text-blue-700 font-serif text-center pb-8">
+                            Welcome to the Balaji Agro
+                        </h1>
+                        <p className="text-base md:text-lg leading-relaxed text-justify font-serif w-full sm:w-auto">
+                            Our Company was incorporated as Balaji Agro Tech as a public limited company, under
+                            the Companies Act, 1956 vide Certificate of Incorporation dated
+                            July 31, 2008. Our Company is in the business of processing of
+                            non-basmati and basmati rice in India.
+                        </p>
+                        <p className="mt-4 md:text-lg leading-relaxed text-justify font-serif w-full sm:w-auto">
+                            We are one of the largest processors of non-basmati rice in the
+                            State of Jharkhand with a milling capacity of 1500 Metric tons per
+                            day, and we market our products across Eastern Indian States, namely
+                            Jharkhand, Bihar, Odisha, West Bengal, and Assam.
+                        </p>
                     </div>
-                </div>
+                    <div className="relative mx-auto overflow-hidden w-full md:max-w-md lg:max-w-lg xl:max-w-xl">
+                        <div className="relative z-10 overflow-hidden rounded-[18px] p-[2px] mt-[8px] w-full">
+                            <img
+                                src="https://images.pexels.com/photos/3345727/pexels-photo-3345727.jpeg?auto=compress&cs=tinysrgb&w=600"
+                                alt="Placeholder"
+                                className="w-full h-[380px] object-cover object-center transform scale-100 transition-transform duration-300 hover:scale-105"
+                            />
+                        </div>
+                    </div>
 
                 </div>
 
