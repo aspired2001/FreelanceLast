ebb55e5f7bd16d7658766b42c3aedf3d562bff0b
aspired2001 <100749624+aspired2001@users.noreply.github.com>
2024-05-31 01:08:51 +0530

    Meesagings

--- src/pages/Carreer.jsx
+++ src/pages/Carreer.jsx
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 
 const Carreer=()=>{
     return(
-        <div>
+        <div >
              
             <div className="relative overflow-hidden">
                 {/* Background image */}
@@ -23,7 +23,7 @@ const Carreer=()=>{
 
             {/* ------------------------ */}
           
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 px-9 pt-20">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 lg:px-36 pt-20">
                 <div className="bg-slate-300 shadow-inner rounded-xl pb-8">
                     <h1 className="max-sm:text-2xl text-3xl font-serif font-semibold text-center pt-4 text-blue-700">CONTACT US</h1>
                     <ContactCarreer />
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -21,7 +21,7 @@ const ContactUs = () => {
             </span>
          </div>
 
-         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 px-9 pt-20">
+         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 lg:px-36 pt-20">
             <div className="bg-slate-300 shadow-inner rounded-xl pb-8">
                <h1 className="text-3xl font-serif font-semibold text-center pt-4 text-blue-700">CONTACT US</h1>
                <ContactForm />
